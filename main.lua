f9f5b430ef868bd223ff0cd2cf0d76103f7a055a
Yomo-blue-archive <bluearchiveyomo@gmail.com>
2026-02-18 21:56:18 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,20 @@
+-- Khai báo biến tốc độ (Bạn có thể thay đổi số 100 thành số khác)
+local speedValue = 100
+
+-- Thông báo cho người dùng biết script đã chạy
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Ys9282 Speed Hack",
+    Text = "Tốc độ hiện tại: " .. speedValue,
+    Duration = 5
+})
+
+-- Vòng lặp để giữ tốc độ luôn ổn định (tránh bị game reset)
+while true do
+    -- Kiểm tra nếu nhân vật tồn tại
+    if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
+        -- Chỉnh tốc độ chạy
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speedValue
+    end
+    task.wait(1) -- Đợi 1 giây rồi lặp lại
+end
 
